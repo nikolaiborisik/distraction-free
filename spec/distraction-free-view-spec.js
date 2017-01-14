4241057891b06ba5314bde53192ab730f87af53d
Nikolai Borisik <n1k0lajby@gmail.com>
2017-01-14 23:10:29 +0300

    Initial

---
+++ spec/distraction-free-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import DistractionFreeView from '../lib/distraction-free-view';
+
+describe('DistractionFreeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
